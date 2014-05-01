a9adbe8b49d263eb4da54c2ed57f6fb35c8e7486
21paradox <1036339815@qq.com>
2014-05-01 17:41:47 +0800

    touch速度优化

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("app", ['ngRoute', 'ngAnimate']);
+var app = angular.module("app", ['ngRoute', 'ngAnimate','ngTouch']);
 
 //url mapping
 app.config(function($routeProvider) {
